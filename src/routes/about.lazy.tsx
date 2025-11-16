4ddab2886b5294d45238ae297c3e0cac3a8dd4df
Praise Jacob <iampraisejacob@gmail.com>
2025-11-17 01:07:00 +0530

    fix: fix the about page

--- src/routes/about.lazy.tsx
+++ src/routes/about.lazy.tsx
@@ -8,11 +8,14 @@ export const Route = createLazyFileRoute("/about")({
 
 function RouteComponent() {
 	return (
-		<div className="flex flex-col items-center justify-center flex-1">
-			<div className="bg-black rounded-2xl p-3 flex items-center justify-center mb-4 shadow-xl">
+		<div className="flex flex-col items-center justify-center flex-1 h-dvh">
+			<div className="flex items-center justify-center py-4 px-6 bg-accent/20 border-accent border-2 rounded-3xl">
 				<img src={logo} alt="Logo" className="h-32 w-32" />
 			</div>
-			<h1 className="text-4xl font-bold text-gray-800">Zap</h1>
+			<h1 className="text-4xl font-bold mt-6">
+				Zap
+				<span className="text-accent font-extrabold">!</span>
+			</h1>
 		</div>
 	);
 }

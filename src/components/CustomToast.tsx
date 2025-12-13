79cac8881dec9f73cd41845bb4659b529b438ff6
Praise Jacob <iampraisejacob@gmail.com>
2025-12-13 16:55:27 +0530

    chore: update toast color pallete

--- src/components/CustomToast.tsx
+++ src/components/CustomToast.tsx
@@ -7,10 +7,11 @@ export function CustomToast() {
 			toastOptions={{
 				duration: 2000,
 				style: {
-					border: "1px solid oklch(0.44 0 0)",
+					border: "1px solid oklch(0.27 0 0)",
 					borderRadius: "12px",
-					background: "oklch(0.27 0 0)",
-					color: "oklch(0.97 0 0)",
+					background: "oklch(0.14 0 0)",
+					color: "oklch(0.92 0 0)",
+					borderWidth: 1,
 				},
 			}}
 		/>

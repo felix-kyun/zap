fa37a142a800db8d438136f689d723f0abf25add
Praise Jacob <iampraisejacob@gmail.com>
2025-12-24 21:18:21 +0530

    fix: remove swc as it causes issues with decorators

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -19,5 +19,8 @@ export default defineConfig({
 		proxy: {
 			"/api": "http://localhost:3000",
 		},
+	},
+	esbuild: {
+		target: "es2024",
 	},
 });
